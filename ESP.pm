5e9505530f3b51ed23ec4b34b905a95cc48d0fda
Tye McQueen <tyemq@cpan.org>
2007-08-10 16:25:09 +0000

    Import of TYEMQ/Acme-ESP-1.001003 from CPAN.
    gitpan-cpan-distribution: Acme-ESP
    gitpan-cpan-version:      1.001003
    gitpan-cpan-path:         TYEMQ/Acme-ESP-1.001003.tar.gz
    gitpan-cpan-author:       TYEMQ
    gitpan-cpan-maturity:     released

--- ESP.pm
+++ ESP.pm
@@ -1,7 +1,7 @@
-package ACME::ESP;
+package Acme::ESP;
 use vars qw( $VERSION @EXPORT );
 BEGIN {
-    $VERSION= 1.001_002;
+    $VERSION= 1.001_003;
     @EXPORT= 8x0 .oO ;
     require Exporter;
     *import= \&Exporter::import;
@@ -12,15 +12,29 @@ use strict;
 sub oO
 {
     my( $thought )= !@_ ? undef : join " ... ", @_;
-    return bless \$thought, 'ACME::ESP::Scanner';
+    return bless \$thought, 'Acme::ESP::Scanner';
 }
 
-package ACME::ESP::Scanner;
+package Acme::ESP::Scanner;
 
 use overload(
     '.' => \&scan,
-    fallback => 0,
+    nomethod => \&explode,
 );
+
+my $fluff= '';
+my $openMind= 5;
+{
+    my $mind= "thoughts";
+    $mind= \$mind;
+    my( $p2, $rc, $f )=
+        unpack "LLL", unpack "P12", pack "L", $mind;
+    my $state= unpack "C", pack "V", $f;
+    if(  $state == $openMind  ) {
+        ++$openMind;
+        $fluff= "x4";
+    }
+}
 
 sub scan
 {
@@ -29,16 +43,19 @@ sub scan
         if  ! $right;
     $mind= \$_[1];
     my $secret= '';
-    my( $p2, $rc, $f )=
-        unpack "LLL", unpack "P12", pack "L", $mind;
-    if(  5 == unpack "C", pack "V", $f  ) {
+    my( $p2, $rc, $f, $p3 )=
+        unpack "L4", unpack "P16", pack "L", $mind;
+    if(  $openMind == unpack "C", pack "V", $f  ) {
         my( $pv, $cur, $siz, $iv )=
-            unpack "L4", unpack "P16", pack "L", $p2;
+            unpack $fluff."L4", unpack "P20", pack "L", $p2;
+        $pv= $p3
+            if  $fluff;
         $secret= unpack "P$iv", pack "L", $pv-$iv;
     }
+    bless $scanner, 'Acme::ESP';
     my $thought= $$scanner;
     if(  defined $thought  ) {
-        # It is surprise how hard it can be to clear your mind
+        # It is surprising how hard it can be to clear your mind
         my $surface= "$$mind";
         $$mind= undef;
         $$mind= $thought . $surface;
@@ -47,17 +64,22 @@ sub scan
     return $secret;
 }
 
+sub explode
+{
+    die "Acme::ESP mis-used.\n";
+}
+
 'relax'
 __END__
 
 =head1 NAME
 
-ACME::ESP - The power to implant and extract strings' thoughts.
+Acme::ESP - The power to implant and extract strings' thoughts.
 
 =head1 SYNOPSIS
 
     #!/usr/bin/perl -l
-    use ACME::ESP;
+    use Acme::ESP;
 
     my $string= "Nice hat.";
 
--- t/descartes.t
+++ t/descartes.t
@@ -1,13 +1,13 @@
 use Test qw( plan ok );
 
 plan(
-    tests => 6,
+    tests => 7,
     todo => [ ],
 );
 
-require ACME::ESP;
+require Acme::ESP;
 ok(1);
-ACME::ESP->import();
+Acme::ESP->import();
 ok(1);
 
 $i= "person";
@@ -18,3 +18,5 @@ ok( exists $Rene{descartes} );
 ok( $i.oO("Did I leave the oven on?"), "I exist" );
 ok( $i.oO(''), '/oven/' );
 ok( $i.oO(''), '' );
+$i= 1;
+ok( ! eval { 0 for $i..oO('Independent thought is fatal'); 1 } );
